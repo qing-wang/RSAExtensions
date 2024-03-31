00ebe4d4e78ec22e11e405400803e1ec1056c061
Qing Wang <qing.chwang@gmail.com>
2024-03-31 20:25:14 +0800

    測試加上 decrypt

--- TestRSAKeyGen/Program.cs
+++ TestRSAKeyGen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Text;
 
 namespace TestRSAKeyGen
@@ -81,7 +82,6 @@ namespace TestRSAKeyGen
                 UseShellExecute = false,
                 Verb = "open",
                 RedirectStandardInput = true,
-                StandardInputEncoding = Encoding.ASCII,
                 RedirectStandardOutput = true,
                 StandardOutputEncoding = Encoding.ASCII,
                 RedirectStandardError = true,
@@ -101,22 +101,69 @@ namespace TestRSAKeyGen
         {
             try
             {
-                string[] keyPair = GenerateKeyPair();
-                Console.WriteLine(keyPair[0]);
-                Console.WriteLine(keyPair[1]);
-                //
-                byte[] b = new byte[64];
-                for (int i = 0; i < b.Length; i++)
-                    b[i] = (byte)i;
-                string s = Encrypt(keyPair[1], b);
-                Console.WriteLine("Encrypt: " + s);
-                byte[] b2 = Convert.FromBase64String(s);
-                //
-                string s2 = Decrypt(keyPair[0], b2);
-                Console.WriteLine("Decrypt: " + s2);
-                byte[] b3 = Convert.FromBase64String(s2);
-                for (int i = 0; i < b3.Length; i++)
-                    Console.WriteLine(b3[i]);
+                if( args.Length < 3)
+                {
+                    Console.WriteLine("args: <generateKey>|<encrypt>|<descrypt> <keyFile> <inputFile> <outputFile>");
+                    Console.WriteLine();
+                    //
+                    string[] keyPair = GenerateKeyPair();
+                    Console.WriteLine(keyPair[0]);
+                    Console.WriteLine(keyPair[1]);
+                    //
+                    byte[] b = new byte[64];
+                    for (int i = 0; i < b.Length; i++)
+                        b[i] = (byte)i;
+                    string s = Encrypt(keyPair[1], b);
+                    Console.WriteLine("Encrypt: " + s);
+                    byte[] b2 = Convert.FromBase64String(s);
+                    //
+                    string s2 = Decrypt(keyPair[0], b2);
+                    Console.WriteLine("Decrypt: " + s2);
+                    byte[] b3 = Convert.FromBase64String(s2);
+                    for (int i = 0; i < b3.Length; i++)
+                        Console.WriteLine(b3[i]);
+                    return;
+                }
+                if (args[0].Equals("generateKey"))
+                {
+                    string[] keyPair = GenerateKeyPair();
+                    Console.WriteLine(keyPair[0]);
+                    Console.WriteLine(keyPair[1]);
+                    //
+                    using (StreamWriter sw = new StreamWriter(args[1]))
+                    {
+                        sw.WriteLine(keyPair[0]);
+                    }
+                    using (StreamWriter sw = new StreamWriter(args[2]))
+                    {
+                        sw.WriteLine(keyPair[1]);
+                    }
+                }
+                else if (args[0].Equals("encrypt"))
+                {
+                    string publicKey = File.ReadAllText(args[1]);
+                    byte[] b = File.ReadAllBytes(args[2]);
+                    byte[] _b = new byte[32];
+                    for (int i = 0; i < _b.Length; i++)
+                        _b[i] = b[i];
+                    string s = Encrypt(publicKey, _b);
+                    Console.WriteLine(s);
+                    byte[] b2 = Convert.FromBase64String(s);
+                    File.WriteAllBytes(args[3], b2);
+                }
+                else if (args[0].Equals("decrypt"))
+                {
+                    string privateKey = File.ReadAllText(args[1]);
+                    byte[] b = File.ReadAllBytes(args[2]);
+                    string s = Decrypt(privateKey, b);
+                    Console.WriteLine(s);
+                    byte[] b2 = Convert.FromBase64String(s);
+                    File.WriteAllBytes(args[3], b2);
+                }
+                else
+                {
+                    Console.WriteLine("args: <generateKey>|<encrypt>|<descrypt> <keyFile> <inputFile> <outputFile>");
+                }
             }
             catch (Exception e)
             {
